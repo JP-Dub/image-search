942297cc502bc717942eec7ddd1afdb3076889c1
Glitch (imaginary-bank) <none>
2018-01-04 21:09:57 +0000

    💉📼 Checkpoint mongo.js:553280

--- mongo.js
+++ mongo.js
@@ -4,7 +4,7 @@ var MongoClient = require('mongodb').MongoClient,
     assert = require('assert'),
     dbName = "urlDatabase",
     app = express();
-
+var seeHistory = {};
 
 exports.search = function(query) {
 // Use connect method to connect to the Server
@@ -14,31 +14,35 @@ exports.search = function(query) {
   
     var db = client.db(dbName);  
     var collection = db.collection('history');
-      
-    if(!"history") {
+    
+    //collection.deleteMany();
+    
+    if(query !== "history") {
       var date = new Date().toString();
       // inserts the newly created short url with the queried url 
-      collection.insertOne({search: query, time: date}, function(err, results) {
+      collection.insertOne({Search: query, Time: date}, function(err, results) {
         assert.equal(err, null);
-        console.log(results, "success");
+        console.log("Mongo has saved the search parameter");
         client.close();          
       }); 
     } else {       
       // checks for url in database     
       collection.find( {},
                       {projection : 
-                       {_id: 0, search: 1, time: 1}
+                       {_id: 0, Search: 1, Time: 1}
                       }, { 
                        sort : 
-                        {time: -1}
+                        {Time: -1}
       }, {
         limit : 10 }).toArray(function(err, history) {
         assert.equal(err, null);
-        console.log(history)
-        return history;
+       seeHistory = history;
+        //return history;
         client.close();
         
       });     
     }
 });
 }
+
+exports.h
