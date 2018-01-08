e83993544312d098b3400df77fba72873b63ba7f
Glitch (imaginary-bank) <none>
2018-01-08 22:23:00 +0000

    👍💈 Checkpoint server.js:553280/256

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 'use strict';
 // init project
-/*var http = "https://www.googleapis.com/customsearch/v1?key=",
-    offset = 10,
-    options = "&num=10&c2coff=1&start=" + offset;*///&hl=en&gl=us&cr=countryUS&searchType=image&siteSearch=items%5B%5D.title%2C%20items%5B%5D.link%2C%20items%5B%5D.imag";
-var apiKEY = process.env.API_KEY,
+var apiKEY = "&key=" + process.env.API_KEY,
     cxENG = "&cx=" + process.env.CX_ENG,
     mongo = require('./mongo'),
     express = require('express'),
     app = express();
-
-//var url = require('url');
-//var myURL = url.parse("https://imaginary-bank.glitch.me/search/cars?offset=2");
-//console.log(myURL)
-
-    
+  
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
@@ -28,21 +20,19 @@ app.get("/", function (request, response) {
 
 app.get("/search/*", function (req, res) {
   var query = req.params[0],
-      offset = req.param('offset');
+      offset = req.param('offset'),
+      options = "&num=10&c2coff=1&start=" 
       if(!offset) {     
         offset = 10;
       }
-  //https://www.googleapis.com/customsearch/v1?q=
-  var http = "https://www.googleapis.com/customsearch/v1?key=" + apiKEY + cxENG,
-      options = "&num=10&c2coff=1&start=" + offset; 
+  var https = "https://www.googleapis.com/customsearch/v1?q=" + query + options + offset + apiKEY + cxENG; 
   //res.sendFile(__dirname + '/views/postsearch.html');
   mongo.search(query, function(err, results) {
   if(err) return console.error(err);
     console.log("saved: ", query);  
   });
-  var url = http + apiKEY + cxENG + "&q=" + query + options;
-  //console.log("url" , url)
-   res.redirect(url)
+
+   res.redirect(https)
 });
     
 // post results of last 10 searches
