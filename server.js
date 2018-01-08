4cc5e0950d7b0c4822791836a094ddbe4612ebfc
Glitch (imaginary-bank) <none>
2018-01-08 17:10:29 +0000

    👘🌻 Checkpoint server.js:553280/43

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get("/search/*", function (req, res) {
   if(err) return console.error(err);
     console.log("saved: ", query);  
   });
-  var url = http + apiKEY + cxENG + "&q=" + query + "&num=10&start=10&linkSite=0&hl=en&c2coff=1&gl=us";
+  var url = http + apiKEY + cxENG + "&q=" + query + "&num=10&start=10&linkSite=0&hl=en&c2coff=1&gl=us&cr=countryUS";
   console.log("url" , url)
    res.redirect(url)
 });
