e6753dd7b5bbc9175600e32540a31af54dd82c6a
Glitch (imaginary-bank) <none>
2018-01-10 21:05:35 +0000

    🍜🌾 Checkpoint server.js:553280/43

--- server.js
+++ server.js
@@ -39,7 +39,8 @@ function wretch(url, baloney) {
 }
 
 app.get("/search/*", function (req, res, next) {
-  var query = req.params[0],
+  var query = req.params[0] + "
+      exact = "&exactTerms=" + query,
       offset = req.query.offset || 10, //10,//offset = req.param('offset'),
       options = "&num=10&c2coff=1&imgColorType=color&client=google-csbe&start="; //&searchType=image
       
