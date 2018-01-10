7341cbc5dee957237cc0e09cb40425e9a5798f4d
Glitch (imaginary-bank) <none>
2018-01-10 21:47:03 +0000

    🌮📍 Checkpoint searchEngine.js:553280/132

--- searchEngine.js
+++ searchEngine.js
@@ -1,11 +1,15 @@
 var fetch = require('node-fetch');
 
 
-exports.engine = function(url, baloney) {
+exports.engine = function(url, complete) {
   fetch(url) 
     .then(function(res) {
       return res.json();
     }).then(function(json) {
-      baloney(json);
+    var json = json[0];
+    for(var key in json)
+    
+    
+      complete(json);
     });
 }
