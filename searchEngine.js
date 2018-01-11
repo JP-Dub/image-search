3198513c55b65a48ea19f3e7fea1573d0652d1eb
Glitch (imaginary-bank) <none>
2018-01-11 17:20:35 +0000

    🙄🏙 Checkpoint searchEngine.js:553280/91

--- searchEngine.js
+++ searchEngine.js
@@ -6,14 +6,16 @@ exports.engine = function(url, complete) {
   fetch(url) 
     .then(function(res) {
     var err = {status: res.staus, statusCode: res.statusCode};
-    if(res.status !== 200) return complete(err);
+    if(res.status !== 200)return complete(err);
+
     //console.log(res.status,"Status", res.json)
       return res.json();
     }).then(function(json) {
-    console.log(json.items.length, "length", json)
+    var jsonp = json.items;
+    console.log(jsonp.length, "length", json)
      /* var obj = [];
       for(var i = 0; i < json.items.length; i++) {
-        var arr = json.items[i],
+        var arr = jsonp[i],
           items = {  imageURL : arr.pagemap.cse_image[0].src,
                      altText: arr.snippet, 
                      pageURL: arr.link                
